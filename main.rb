4af1da40bba0dd3d25e6b05844e5a8637e032cca
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2023-04-16 10:36:46 -0300

    Add main program file

---
+++ main.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+filename = ARGV.first
+
+if filename && ['input1.txt', 'input2.txt', 'input3.txt'].include?(filename)
+  # Do something
+else
+  puts "Pass in either input1.txt, input2.txt or input3.txt"
+end
