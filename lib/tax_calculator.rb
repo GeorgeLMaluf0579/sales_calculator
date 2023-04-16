9810b45e41388e481e9ebbdb4eee0faab327710a
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2023-04-16 12:05:03 -0300

    Add TaxCalculator class

---
+++ lib/tax_calculator.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class TaxCalculator
+  def initialize(base_tax, import_tax)
+    @base_tax = base_tax
+    @import_tax = import_tax
+  end
+
+  def calculate_tax(prince, imported, tax_exempt)
+    tax = 0
+    tax += @base_tax unless tax_exempt
+    tax += @import_tax if imported
+    round_up(price * tax)
+  end
+
+  private
+
+  def round_up(value, step = 0.05)
+    (value * (1.0 / step)).ceil / (1.0 / step)
+  end
+end
