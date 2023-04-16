ee3f07629983dacd5ac24cd172fc6a3105ee74c0
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2023-04-16 10:47:44 -0300

    Add SalesComputer class

---
+++ lib/sales_computer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal:  true
+
+class SalesComputer
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def compute_and_print
+    print 'here is the results'
+  end  
+end
--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@
 filename = ARGV.first
 
 if filename && ['input1.txt', 'input2.txt', 'input3.txt'].include?(filename)
-  # Do something
+  SalesComputer.new(filename).compute_and_print
 else
   puts "Pass in either input1.txt, input2.txt or input3.txt"
 end
